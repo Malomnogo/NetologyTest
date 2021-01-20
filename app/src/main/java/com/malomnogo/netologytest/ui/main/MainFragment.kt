664e3bd479cca8d753138c68957d5d7ddfadba2c
eugene <auricamercer@gmail.com>
2021-01-21 00:12:24 +0300

    upd -- single activity

---
+++ app/src/main/java/com/malomnogo/netologytest/ui/main/MainFragment.kt
@@ -0,0 +1,42 @@
+package com.malomnogo.netologytest.ui.main
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
+import com.malomnogo.netologytest.R
+import com.malomnogo.netologytest.model.ui.NetologyUiData
+import com.malomnogo.netologytest.utils.Status
+import kotlinx.android.synthetic.main.fragment_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+    private val viewModel: MainViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.dataUi.observe(viewLifecycleOwner) { initUi(it) }
+        viewModel.state.observe(viewLifecycleOwner) { handleStatus(it.getContentIfNotHandled()) }
+    }
+
+    private fun handleStatus(status: Status?) {
+        fragment_main_pb.visibility = View.GONE
+
+        when (status) {
+            Status.ERROR -> showErrorResult(getString(R.string.status_error))
+            else -> { /*do nothing */ }
+        }
+    }
+
+    private fun showErrorResult(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initUi(listNetologyData: List<NetologyUiData>) {
+
+        with(fragment_main_rv_main_data) {
+            adapter = MainAdapter(listNetologyData)
+        }
+    }
+}
